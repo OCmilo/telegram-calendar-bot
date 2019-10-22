8f3d6d202e724480d8e92602e249a9cd94f6064b
Camilo Carneiro <camilocarneiro@hotmail.com>
2019-10-22 12:22:21 -0300

    improved config.py structure

--- lib/config.py
+++ lib/config.py
@@ -1,9 +1,8 @@
-#Telegram configurations
 TOKEN = '838321161:AAGu3-keIMZpWjiOaYEbZmta0TgcD4sh41s'
-TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
-
 WEBHOOK_HOSTNAME = 'fateor'
-WEBHOOK_URL = 'https://fateor.serveo.net'
+
+TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
+WEBHOOK_URL = 'https://{}.serveo.net'.format(WEBHOOK_HOSTNAME)
 
 WEBHOOK_ENDPOINT = '{}/webhook'.format(WEBHOOK_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(TELEGRAM_URL, WEBHOOK_ENDPOINT)
