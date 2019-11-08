ce3fefd6fefead2b79be7fcca41b1c9e3ce7c758
Camilo Carneiro <camilocarneiro@hotmail.com>
2019-11-07 21:46:38 -0300

    minor improvements

--- lib/bot.py
+++ lib/bot.py
@@ -9,12 +9,12 @@ class TelegramBot:
     def __init__(self):
         #initializing fields for parsing
         self.chat_id = None
-        self.text = None
+        self.incoming_message_text = None
         self.first_name = None
         #creating Google Calendar API Service
         self.service = authentication()
 
-
+    #Store received JSON in data attributes
     def parseData(self, data):
         response = data['message']
 
@@ -24,6 +24,7 @@ class TelegramBot:
 
     def __sendMessage(self):
         res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(self.chat_id, self.outgoing_message_text)) 
+
         return res.status_code == 200
 
     def conditionalResponse(self):
